212830bf877c06157f3b3fddec78f30ef83eaf4b
generalgithup-ui <generalgithup@gmail.com>
2026-09-01 02:04:53 -1200

    Change this code already

---
+++ Write code self.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class Admin{
+	private :
+		char email[40],pw[40];
+	public:
+              void InputAdmin(){
+                  cin.ignore();
+                  cout<<"Enter thje email admin :"; 
+                  cin.getline(email,40);
+                  cout<<"Enter "
+                  
+		void     	
+		
+		}
+    }
+
+		
+};
+int main(){
+	
+	
+	
+	return 0;
+}
